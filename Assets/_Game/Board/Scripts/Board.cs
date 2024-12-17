f6a7991983f655e71a45cc8dd9c7d7caa4387b48
Datta <73988634+tathanhdatt@users.noreply.github.com>
2024-12-17 16:42:17 +0700

    Create base board

---
+++ Assets/_Game/Board/Scripts/Board.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Dt.Attribute;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    [SerializeField, Required]
+    private Cell prefab;
+
+    private float CellWidth => this.prefab.RectTransform.rect.width;
+    private float CellHeight => this.prefab.RectTransform.rect.height;
+
+    private Cell[,] cells;
+    
+
+    private void Awake()
+    {
+        Initialize();
+    }
+
+    public void Initialize()
+    {
+        GenerateCells();
+    }
+
+    private void GenerateCells()
+    {
+        this.cells = new Cell[BoardConstant.boardSize, BoardConstant.boardSize];
+        for (int i = 0; i < BoardConstant.boardSize; i++)
+        {
+            for (int j = 0; j < BoardConstant.boardSize; j++)
+            {
+                Cell newCell = Instantiate(this.prefab, this.transform);
+                this.cells[i, j] = newCell;
+                SetCellPosition(i, j);
+            }
+        }
+    }
+
+    private void SetCellPosition(int row, int column)
+    {
+        const int middle = BoardConstant.boardSize / 2;
+        Vector3 position = Vector3.zero;
+        position.x = CellWidth * (row - middle) + CellWidth / 2;
+        position.y = CellHeight * (column - middle) + CellHeight / 2;
+        this.cells[row, column].RectTransform.localPosition = position;
+    }
+}
---
+++ Assets/_Game/Board/Scripts/Editor/CellGraphicContainerEditor.cs
@@ -0,0 +1,38 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(CellGraphicContainer))]
+public class CellGraphicContainerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        CellGraphicContainer container = target as CellGraphicContainer;
+        if (container == null) return;
+        GUILayout.BeginHorizontal();
+        bool isClickAddButton = GUI.Button(new Rect(10, 10, 100, 30), "Add");
+        bool isClickRemoveButton = GUI.Button(new Rect(120, 10, 100, 30), "Remove");
+        GUILayout.EndHorizontal();
+        EditorGUILayout.Space(50);
+        if (isClickAddButton)
+        {
+            container.items.Add(new CellGraphicItem());
+        }
+        else if (isClickRemoveButton)
+        {
+            if (container.items.Count > 0)
+            {
+                container.items.RemoveAt(container.items.Count - 1);
+            }
+        }
+
+        foreach (CellGraphicItem item in container.items)
+        {
+            EditorGUILayout.BeginHorizontal();
+            item.id = (CellGraphicID)EditorGUILayout.EnumPopup(item.id);
+            EditorGUILayout.Space(1);
+            item.graphic = (Sprite)EditorGUILayout.ObjectField(item.graphic, typeof(Sprite), false);
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.Space(5);
+        }
+    }
+}
